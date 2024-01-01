5c6df42930a8c94808d0d010fcbcedea56e3c856
danielobare <dnobare@gmail.com>
2024-01-01 16:33:27 +0000

    Added python311 requirement to nix file

--- replit.nix
+++ replit.nix
@@ -1,8 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
-
 pkgs.mkShell {
   buildInputs = [
     pkgs.wget
     pkgs.python311
   ];
+
+  shellHook = ''
+    export PYTHON="${pkgs.python311}/bin/python"
+  '';
 }
