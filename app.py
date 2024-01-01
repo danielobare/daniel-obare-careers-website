7092539232538fc60f06b49fcfd3810533c1bcba
danielobare <dnobare@gmail.com>
2024-01-01 13:53:07 +0000

    Added aesthetic changes, buttons, handled errors arising from salary missing

--- app.py
+++ app.py
@@ -2,10 +2,32 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+JOBS = [{
+    'id': 1,
+    'title': 'Python Developer',
+    'location': 'New York',
+    'salary': 50000
+}, {
+    'id': 2,
+    'title': 'Front Developer',
+    'location': 'Nairobi',
+    'salary': 40000
+}, {
+    'id': 3,
+    'title': 'Backend Engineer',
+    'location': 'Capetown',
+    'salary': 150000
+}, {
+    'id': 4,
+    'title': 'Java Developer',
+    'location': 'San Fransisco',
+    'salary': 250000
+}]
+
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='danielobare')
 
 
 if __name__ == "__main__":
