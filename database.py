b3d5a7efe23f768624474cf9b3fedf6a979d7fdc
Daniel Obare Nyakundi <dnobare@gmail.com>
2024-12-30 00:00:33 +0300

    removing accidentally included secret

--- database.py
+++ database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 import os
 
-# db_connection_string = os.environ['DB_CONNECTION_STRING']
-db_connection_string = 'mysql+pymysql://careersadmin:Cont0546.obare_41@careers-mysql-flexible.mysql.database.azure.com:3306/danielobarecareers?connect_timeout=30'
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
