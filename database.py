a8f546eca9b1f5d4a1168af0d2c251059d9f0f97
Daniel Obare Nyakundi <dnobare@gmail.com>
2024-12-29 19:02:08 +0300

    removed sql dump, pscale.deb, and csv files

--- database.py
+++ database.py
@@ -5,5 +5,5 @@ db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
-                           "ssl_ca": "/etc/ssl/cert.pem"
+                           "ssl_ca": "certs/DigiCertGlobalRootCA.crt.pem"
                        }})
