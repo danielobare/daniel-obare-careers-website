1ea960c8989e48289903fb5218b869dc94368f11
Daniel Obare Nyakundi <dnobare@gmail.com>
2024-12-29 23:58:12 +0300

    resolving accidental secrets release

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 import os
 
-db_connection_string = os.environ['DB_CONNECTION_STRING']
+# db_connection_string = os.environ['DB_CONNECTION_STRING']
+db_connection_string = 'mysql+pymysql://careersadmin:Cont0546.obare_41@careers-mysql-flexible.mysql.database.azure.com:3306/danielobarecareers?connect_timeout=30'
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
